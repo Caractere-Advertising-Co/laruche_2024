0cfe695df9746dc476c234df552b7169150fb1e5
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2024-11-06 14:09:09 +0100

    Correction card-to-listing

--- templates-parts/section-card-to-listing.php
+++ templates-parts/section-card-to-listing.php
@@ -8,7 +8,7 @@
 
                 <div class="col" style="background-image:url('<?php if($bg): echo $bg['url'];endif;?>');">
                     <?php if($title): echo $title; endif;?>
-                    <?php if($cta): echo '<a href="'.$cta['url'].'" class="cta">'.$cta['title'].'</a>'; endif;?>
+                    <?php if($cta): echo '<a href="'.$cta['url'].'" class="cta">'.$cta['title'].'<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></a>'; endif;?>
                 </div>
             
             <?php endwhile;
