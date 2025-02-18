d159e548e2bd618dbf53f1c069db9388df9bc20d
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-02-18 16:43:21 +0100

    $thb

--- templates-parts/section-listing-bien.php
+++ templates-parts/section-listing-bien.php
@@ -95,7 +95,7 @@ endif;
             ?>
                 <div class="card">
                     <?php echo in_array($statut,$validLink) ? '' :  '<a href="'.get_permalink().'">'; ?>
-                        <div class="block-img miniature-bien" <?php if($thb):?>style="background-image:url('<?php echo $thb['url'];?>');"<?php endif;?>>
+                        <div class="block-img miniature-bien" <?php if($thb):?>style="background-image:url('<?php echo $thb;?>');"<?php endif;?>>
                             <?php if($new):?><span class="statut">New</span><?php endif;?>
                             <?php if($statut):
                                 if($statut == "Vendu"):?>
