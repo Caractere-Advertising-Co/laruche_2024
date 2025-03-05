bc238441a35e2f0a1692c6525eb321d43120d73d
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-05 16:43:39 +0100

    contact

--- contact.php
+++ contact.php
@@ -18,7 +18,7 @@ $form2       = get_field('formulaire-recherche');
 ?>
 
 <section id="contact-introduction">
-    <div class="container columns <?php if(is_page(45385)): echo '-row'; endif;?>">
+    <div class="container columns <?php if(is_page(45385)|| is_page(627)): echo '-row'; endif;?>">
         <div class="colg">
             <div class="title titlelvl2">
                 <?php if($surtitre): echo '<h1>'.$surtitre.'</h1>'; endif;?>
@@ -29,12 +29,19 @@ $form2       = get_field('formulaire-recherche');
             <div class="contactInfos"><?php if($infoContact): echo $infoContact; endif; ?></div>
         </div>
         <div class="cold">
+            <?php if(is_page(45385) || is_page(627)):?>
+                <div class="fleche-form">
+                    <div class="block-img"><img src="http://laruche-copie.caractere-advertising.be/wp-content/uploads/2025/03/fleche_gauche.png" alt="laruche_fleche_gauche"/></div>
+                    <h2>Quel est <strong>votre projet ?</strong></h2>
+                    <div class="block-img"><img src="http://laruche-copie.caractere-advertising.be/wp-content/uploads/2025/03/fleche_droite.png" alt="laruche_fleche_droite"/></div>
+                </div>
+            <?php endif;?>
             <div class="cta-form columns">
-                <?php if($cta1):?><a href="#!" id="ctaVente" class="cta"><?php echo $cta1['title'];?></a><?php endif;?>
+                <?php if($cta1):?><a href="#!" id="ctaVente" class="cta inactif"><?php echo $cta1['title'];?></a><?php endif;?>
                 <?php if($cta2):?><a href="#!" id="ctaSearch" class="cta cta-dark inactif"><?php echo $cta2['title'];?></a><?php endif;?>
             </div>
             <section id="formulaire">
-                <?php if($form): echo '<div id="formulaire-vendre" class="active">'.do_shortcode( $form ).'</div>'; endif;?>
+                <?php if($form): echo '<div id="formulaire-vendre">'.do_shortcode( $form ).'</div>'; endif;?>
                 <?php if($form2): echo '<div id="formulaire-recherche">'.do_shortcode( $form2 ).'</div>'; endif;?>
             </section>
         </div>
