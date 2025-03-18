329580b0ab38c1dd1e8484262992f319aef04cb3
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-18 08:56:13 +0100

    vardump

--- single-biens.php
+++ single-biens.php
@@ -94,6 +94,7 @@ endif;
                 value=""><?php _e('Fiche du bien', 'ajd') ?></button>
         </form>
         <?php if($ctaNotaire && ($typeBien != "A louer")):?>
+            <?php var_dump($typeBien);?>
             <a href="<?php echo $ctaNotaire['url'];?>" class="ctanotaire cta" target="_blank"><?php echo $ctaNotaire['title'];?></a>
         <?php endif;?>
     </div>
