3645a14c243497953b923dc56e9832eaa7324605
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-03-18 14:06:25 +0100

    adbr

--- templates-parts/section-listing-bien.php
+++ templates-parts/section-listing-bien.php
@@ -111,7 +111,7 @@ endif;
                                         break;
                                     
                                     case 'Sous option':
-                                        echo '<span class="statutBien ssOption">Sous option</span>';
+                                        echo '<span class="statutBien ssOption">Sous <br> option</span>';
                                         break;
 
                                     case 'Loué':
