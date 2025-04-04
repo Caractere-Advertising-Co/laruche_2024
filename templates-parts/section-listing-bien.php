bdca71aa236680bb7b6c3e6af84c3f7b671ed1fc
caractere-advertising <86592307+caractere-advertising@users.noreply.github.com>
2025-04-04 10:05:53 +0200

    section listing bien

--- templates-parts/section-listing-bien.php
+++ templates-parts/section-listing-bien.php
@@ -13,32 +13,66 @@ endif;
 
 $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
-if(is_front_page()):
-    $args = array(
-        'post_type' => 'biens',
-        'post_status' => 'publish',
-        'posts_per_page' => 6,
-        'paged' => $paged
-    );
-else : 
-    $args = array(
-        'post_type'      => 'biens',
-        'post_status'    => 'publish',
-        'paged'          => $paged,
-        'posts_per_page' => 15, // Affichage en grille de 16
-        'meta_query'     => array(
-            array(
-                'key'     => 'type_de_bien',
-                'value'   => $type,
-                'compare' => '='
-            )
-        ),
-        'meta_key'       => 'prix',
-        'orderby'        => 'meta_value_num',
-        'order'          => 'ASC'
-    );
-endif;
+// Requête : récupérer tous les biens pour les trier ensuite
+$args = array(
+    'post_type'      => 'biens',
+    'post_status'    => 'publish',
+    'posts_per_page' => -1,
+    'meta_query'     => array(
+        array(
+            'key'     => 'type_de_bien',
+            'value'   => $type,
+            'compare' => '='
+        )
+    )
+);
 
+$biens_query = new WP_Query($args);
+
+$recentAndActive = [];
+$oldSold = [];
+
+if ($biens_query->have_posts()) {
+    while ($biens_query->have_posts()) {
+        $biens_query->the_post();
+
+        $statut      = get_field('statut_bien');
+        $prix        = get_field('prix');
+        $publishDate = get_the_date('Ymd');
+        $daysDiff    = intval((strtotime(date('Ymd')) - strtotime($publishDate)) / (60 * 60 * 24));
+
+        $group = 'recentAndActive';
+        if ($statut === 'Vendu' && $daysDiff > 180) {
+            $group = 'oldSold';
+        }
+
+        $postData = [
+            'ID'    => get_the_ID(),
+            'prix'  => $prix ? intval($prix) : 0,
+            'post'  => $post,
+        ];
+
+        if ($group === 'recentAndActive') {
+            $recentAndActive[] = $postData;
+        } else {
+            $oldSold[] = $postData;
+        }
+    }
+
+    usort($recentAndActive, fn($a, $b) => $a['prix'] <=> $b['prix']);
+    usort($oldSold, fn($a, $b) => $a['prix'] <=> $b['prix']);
+
+    $allPosts = array_merge($recentAndActive, $oldSold);
+
+    $per_page = is_front_page() ? 6 : 15;
+    $total_posts = count($allPosts);
+    $total_pages = ceil($total_posts / $per_page);
+
+    $offset = ($paged - 1) * $per_page;
+    $displayPosts = array_slice($allPosts, $offset, $per_page);
+}
+
+wp_reset_postdata();
 ?>
 
 <section id="listing-biens">
@@ -55,67 +89,50 @@ endif;
                     echo '<h1>Nos <strong>biens à louer</strong></h1>';
                 endif; ?>
             </div>
-
-            <div class="columns cta-biens">
-                <?php
-                $biens = new WP_Query($args);
-                if($biens->have_posts()):
-                    $total_pages = $biens->max_num_pages;
-
-                    if ($total_pages > 1){
-                        $current_page = max(1, get_query_var('paged'));
-
-                        echo paginate_links(array(
-                            'base' => get_pagenum_link(1) . '%_%',
-                            'format' => '/page/%#%',
-                            'current' => $current_page,
-                            'total' => $total_pages
-                        ));
-                    }
-                endif;?>
-            </div>
         </div>
-    <?php endif;
+    <?php endif; ?>
 
-    $biens = new WP_Query($args);
-
-    if ($biens->have_posts()) : ?>
+    <?php if(!empty($displayPosts)) : ?>
         <div class="container grid grid-biens">
-            <?php while ($biens->have_posts()) : $biens->the_post();
-               $title      = get_the_title();
-                $thb       = get_field('miniature');
-                $tyPEB     = get_field('type_peb');
-                $statut    = get_field('statut_bien');
-                $peb       = get_field('PEB');
-                $pebDble   = get_field('PEB_double');
-                $typeBien  = get_field('type_de_bien');
-                $categorie = get_field('categorie_bien');
-                $lieu      = get_field('situation_lieu');
-                $prix      = get_field('prix');
-                $compBien  = get_field('composition_du_bien');
-                $chambre   = get_field('chambres');
-                $surfHab   = get_field('surf_habitable');
-                $surfTot   = get_field('surf_totale');
-                $fairOff   = get_field('faireOffre');
-                $new       = get_field('new');
-                $validLink = array('Vendu','Loué');
+            <?php 
+            foreach ($displayPosts as $postData) :
+                $post = $postData['post'];
+                setup_postdata($post);
 
-                $aLouer = array('À louer','A louer');
+                $title      = get_the_title();
+                $thb        = get_field('miniature');
+                $tyPEB      = get_field('type_peb');
+                $statut     = get_field('statut_bien');
+                $peb        = get_field('PEB');
+                $pebDble    = get_field('PEB_double');
+                $typeBien   = get_field('type_de_bien');
+                $categorie  = get_field('categorie_bien');
+                $lieu       = get_field('situation_lieu');
+                $prix       = get_field('prix');
+                $chambre    = get_field('chambres');
+                $surfHab    = get_field('surf_habitable');
+                $surfTot    = get_field('surf_totale');
+                $fairOff    = get_field('faireOffre');
+                $validLink  = array('Vendu','Loué');
+
+                $publishDate = get_the_date('Ymd');
+                $currentDate = date('Ymd');
+                $daysDiff    = intval((strtotime($currentDate) - strtotime($publishDate)) / (60 * 60 * 24));
+
+                $isNew = $daysDiff <= 30;
             ?>
                 <div class="card">
                     <?php echo in_array($statut,$validLink) ? '' :  '<a href="'.get_permalink().'">'; ?>
                         <div class="block-img miniature-bien" <?php if($thb):?>style="background-image:url('<?php echo $thb;?>');"<?php endif;?>>
-                            <?php if($new):?><span class="statut">New</span><?php endif;?>
+                            <?php if($isNew):?><span class="statut">Nouveau</span><?php endif;?>
                             <?php if($statut):
                                 switch($statut){
                                     case 'Vendu':   
                                         echo '<span class="statutBien">Vendu !</span>';
                                         break;
-                                    
                                     case 'Sous option':
                                         echo '<span class="statutBien ssOption">Sous <br> option</span>';
                                         break;
-
                                     case 'Loué':
                                         echo '<span class="statutBien">Loué !</span>';
                                         break;
@@ -123,7 +140,7 @@ endif;
                             endif;?>
 
                             <div class="logo_peb">
-                                <?php if($tyPEB && $pebDble && $peb != 'Ne s\'applique pas'): ?>
+                                <?php if($tyPEB && $pebDble): ?>
                                     <img src="<?php echo get_template_directory_uri().'/assets/images/20px_bi/'. $pebDble.'.png';?>" alt="<?php echo $pebDble;?>" />
                                 <?php elseif($peb): ?>
                                     <img src="<?php echo get_template_directory_uri().'/assets/images/20px_un/'. $peb.'.png';?>" alt="<?php echo $peb;?>" />
@@ -140,32 +157,25 @@ endif;
                             <?php 
                                 echo $fairOff ? 'FO àpd ' : ''; 
                                 if($prix): echo $prix ; endif;
-                                echo in_array($typeBien,$aLouer) ? '€ / mois' : ' €';    
+                                echo ($typeBien == 'À louer') ? '€ / mois' : ' €';    
                             ?>
                         </div>
                     <?php echo in_array($statut,$validLink) ?  '' : '</a>' ; ?>
                 </div>
-            <?php endwhile;
-        echo '</div>';
-        endif;
-
-    wp_reset_postdata();?>
-
-    <?php if(!is_front_page(  )):?>
-        <div class="container columns cta-biens number-listing">
-            <?php if(!$biens->have_posts()) { echo '<p>Aucun bien trouvé.</p>'; } ?>
-            <?php 
-                $total_pages = $biens->max_num_pages;
-                if ($total_pages > 1){
-                    $current_page = max(1, get_query_var('paged'));
-                    echo paginate_links(array(
-                        'base' => get_pagenum_link(1) . '%_%',
-                        'format' => '/page/%#%',
-                        'current' => $current_page,
-                        'total' => $total_pages
-                    ));
-                }
-            ?>
+            <?php endforeach; ?>
         </div>
-    <?php endif;?>
+    <?php else : ?>
+        <div class="container"><p>Aucun bien trouvé.</p></div>
+    <?php endif; ?>
+
+    <?php if (!is_front_page() && $total_pages > 1): ?>
+        <div class="container columns cta-biens number-listing">
+            <?php echo paginate_links(array(
+                'base' => get_pagenum_link(1) . '%_%',
+                'format' => '/page/%#%',
+                'current' => $paged,
+                'total' => $total_pages
+            )); ?>
+        </div>
+    <?php endif; ?>
 </section>
